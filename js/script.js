d917882d27bdd2e12c06e0d4bed11c19a863e2da
jp117 <john.paradise117@gmail.com>
2024-03-28 20:29:54 -0400

    added calculate function

--- js/script.js
+++ js/script.js
@@ -1,3 +1,14 @@
+let firstNumber, operator, secondNumber;
+
+function calculate(firstNumber, operator, secondNumber){
+  switch(operator){
+    case 'add': return add(firstNumber, secondNumber);
+    case 'subtract': return subtract(firstNumber, secondNumber);
+    case 'multiply': return multiply(firstNumber, secondNumber);
+    case 'divide': return divide(firstNumber, secondNumber);
+  }
+}
+
 function add(one, two){
   return one + two;
 }
