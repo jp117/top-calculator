af00b47bbbacb5026a49c9650bde859fc3bec746
johnparadise <john.paradise117@gmail.com>
2024-04-03 20:32:11 -0400

    Implemented setNumber and Math.  Calculator works.  Extra Credit not done.

--- js/script.js
+++ js/script.js
@@ -25,36 +25,44 @@ numbers.forEach(function(current){
 
 let addButton = document.getElementById("add");
 addButton.addEventListener('click', function(){
+  setNumber();
+  doMath();
   operator = "add";
 })
 
 let divideButton = document.getElementById("divide");
 divideButton.addEventListener('click', function(){
-  operator = 'divide';
+  setNumber();
+  doMath();
+  operator = "divide";
 })
 
 let multiplyButton = document.getElementById("multiply");
 multiplyButton.addEventListener('click', function(){
+  setNumber();
+  doMath();
   operator = 'multiply';
 })
 
-let subtractButton = document.getElementById("divide");
+let subtractButton = document.getElementById("subtract");
 subtractButton.addEventListener('click', function(){
+  setNumber();
+  doMath();
   operator = "subtract";
 })
 
 let equalsButton = document.getElementById("equals");
 equalsButton.addEventListener('click', function(){
-  displayNumber = calculate(firstNumber, operator, secondNumber);
-  display[0].innerText = displayNumber;
+  setNumber();
+  doMath();
 })
 
 function calculate(firstNumber, operator, secondNumber){
   switch(operator){
-    case 'add': return add(firstNumber, secondNumber);
-    case 'subtract': return subtract(firstNumber, secondNumber);
-    case 'multiply': return multiply(firstNumber, secondNumber);
-    case 'divide': return divide(firstNumber, secondNumber);
+    case 'add': return add(Number(firstNumber), Number(secondNumber));
+    case 'subtract': return subtract(Number(firstNumber), Number(secondNumber));
+    case 'multiply': return multiply(Number(firstNumber), Number(secondNumber));
+    case 'divide': return divide(Number(firstNumber), Number(secondNumber));
   }
 }
 
@@ -72,5 +80,34 @@ function multiply(one, two){
 
 function divide(one, two){
   return one / two;
+}
+
+function setNumber(){
+  if (first && second){
+    second = undefined;
+    displayNumber = "";
+    console.log(`top, first = ${first}`);
+    console.log(`top, second = ${second}`);
+  } else if (!first) {
+    first = displayNumber;
+    displayNumber = "";
+    console.log(`middle, first = ${first}`);
+    console.log(`middle, second = ${second}`);
+  } else if (!second){
+    second = displayNumber;
+    displayNumber = "";
+    console.log(`bottom, first=${first}`);
+    console.log(`bottom, second = ${second}`);
+  }
+}
+
+function doMath(){
+  if (first && second && operator != ""){
+    displayNumber = calculate(first, operator, second);
+    display[0].innerText = displayNumber;
+    first = displayNumber;
+    second = undefined;
+    displayNumber = "";
+  }
 }
 
